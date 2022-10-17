f58cde6a0187c37fde7b8e8f7516b2828bd82784
Kafuu Neko <kafuuneko@gmail.com>
2022-10-17 09:51:23 +0800

    Update LoginAdminister.kt

--- src/main/kotlin/cc/kafuu/jobcollection/servlet/administer/LoginAdminister.kt
+++ src/main/kotlin/cc/kafuu/jobcollection/servlet/administer/LoginAdminister.kt
@@ -1,5 +1,7 @@
 package cc.kafuu.jobcollection.servlet.administer
 
+import cc.kafuu.jobcollection.Application
+import cc.kafuu.jobcollection.utils.JsonUtils
 import javax.servlet.annotation.WebServlet
 import javax.servlet.http.HttpServlet
 import javax.servlet.http.HttpServletRequest
@@ -13,5 +15,25 @@ class LoginAdminister: HttpServlet() {
 
     override fun doPost(req: HttpServletRequest?, resp: HttpServletResponse?) {
         super.doPost(req, resp)
+
+        val username = req?.getParameter("username")
+        val password = req?.getParameter("password")
+        if (username == null || password == null) {
+            resp?.writer?.println(JsonUtils.makeBaseResultJson(-1, "参数不完整").toString())
+            return
+        }
+
+        if (Application.users[username]?.password != password) {
+            resp?.writer?.println(JsonUtils.makeBaseResultJson(-1, "用户名或密码错误").toString())
+            return
+        }
+
+        val token = Application.token.newToken()
+        Application.token.put(token, "username", username)
+
+        resp?.writer?.println(JsonUtils.makeBaseResultJson(0, "登录成功").apply {
+            addProperty("token", token)
+        }.toString())
+
     }
 }
